b6c49b0b3ccb0d3f9747a82df3a97057c3b296ac
Michael Gruper <sarge6411@icloud.com>
2025-02-19 07:27:05 -0600

    Initial Commit

---
+++ RealQuickGameGruperTests/RealQuickGameGruperTests.swift
@@ -0,0 +1,17 @@
+//
+//  RealQuickGameGruperTests.swift
+//  RealQuickGameGruperTests
+//
+//  Created by MICHAEL GRUPER on 2/19/25.
+//
+
+import Testing
+@testable import RealQuickGameGruper
+
+struct RealQuickGameGruperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
